24188a9b842cf668ddc83c97b51a26ba12dc4148
aienmii <anastasiia.bohatyr@student.pk.edu.pl>
2026-03-20 16:47:22 +0100

    first commit

---
+++ src/main/java/pk/ni/pasir_anastasiia_bohatyr/PaSiRAnastasiiaBohatyrApplication.java
@@ -0,0 +1,13 @@
+package pk.ni.pasir_anastasiia_bohatyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaSiRAnastasiiaBohatyrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaSiRAnastasiiaBohatyrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pk/ni/pasir_anastasiia_bohatyr/PaSiRAnastasiiaBohatyrApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.ni.pasir_anastasiia_bohatyr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaSiRAnastasiiaBohatyrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
